021217c9ab2c78f970f4e1e881ccaa919a73f00e
pasko <pasko@DESKTOP-PASKO>
2021-02-18 22:22:32 +0100

    netlify config

--- src/components/SearchCourse.js
+++ src/components/SearchCourse.js
@@ -26,7 +26,7 @@ export default class SearchCourse extends Component {
     }
 
       componentDidMount() {
-        axios.get('/wp-json/wp/v2/posts')
+        axios.get('/cors-proxy/https://couponscorpion.com/wp-json/wp/v2/posts?per_page=80&_fields=title,modified,id,link&order=des')
         .then(res => {
           console.log(res.data)
           console.log(typeof(res.data))
